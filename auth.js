fb91ddbac8d53151aa456b539dc99983ff7b8291
SooryanKaimal <sooryankaimaln14@gmail.com>
2026-02-26 16:18:52 +0530

    Add files via upload

--- auth.js
+++ auth.js
@@ -8,8 +8,11 @@ const loginBtn = document.getElementById('login-btn');
 const signupBtn = document.getElementById('signup-btn');
 const errorMsg = document.getElementById('error-msg');
 
+// Redirect if already logged in
 onAuthStateChanged(auth, (user) => {
-    if (user && window.location.pathname.includes('index.html')) {
+    const path = window.location.pathname;
+    // This checks for BOTH the standard URL and Vercel's clean URL
+    if (user && (path === '/' || path.includes('index.html'))) {
         window.location.href = 'dashboard.html';
     }
 });
